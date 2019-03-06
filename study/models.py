11b01eafb9063e643ed60c5191c7edacd693ecef
divyanshu <dgdivyanshu@gmail.com>
2019-03-06 19:10:28 +0530

    Corrected Models.py

--- study/models.py
+++ study/models.py
@@ -13,7 +13,6 @@ class WordType(models.Model):
 
 
 class WordList(models.Model):
-	word_type = models.ForeignKey(WordType, on_delete=models.CASCADE)
 	word= models.CharField(max_length=30)
 	definition= models.CharField(max_length=200)
 	word_id=models.CharField(max_length=100,primary_key=True)
@@ -26,7 +25,7 @@ class Progress(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	word_id= models.ForeignKey(WordList,on_delete=models.CASCADE)
 	learned= models.BooleanField(default=False)
-	correct=models.IntegerField(default=o0)
+	correct=models.IntegerField(default=0)
 	wrong=models.IntegerField(default=0)
 	interval= models.DateTimeField(default=timezone.now)
 
