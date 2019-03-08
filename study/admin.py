dfef8c3d10590058c315c4a94d092a7395ca185e
divyanshu <dgdivyanshu@gmail.com>
2019-03-08 14:02:01 +0530

    Registered List model and Wordlist

--- study/admin.py
+++ study/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(WordList)
+admin.site.register(List)
 admin.site.register(Progress)
+admin.site.register(WordList)
