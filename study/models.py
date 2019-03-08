4125c5b37f7e2fe2999b417936ecfcae1c7f341a
Rohan_PP <rohanpjain879@gmail.com>
2019-03-08 13:01:14 +0530

    Reverted Changes and Requested Changes done

--- study/models.py
+++ study/models.py
@@ -6,35 +6,28 @@ from django.utils import timezone
 #class Study(models.Model):
 
 class List(models.Model):
-	list_type = models.CharField(max_length=10)
+    list_type = models.CharField(max_length=10)
 
-	def __str__(self):
-		return self.list_type
+    def __str__(self):
+        return self.list_type
 
 
 class WordList(models.Model):
-<<<<<<< HEAD
     word = models.CharField(max_length=30)
     definition = models.CharField(max_length=200)
     word_id = models.CharField(max_length=100, primary_key=True)
-=======
-	word= models.CharField(max_length=30)
-	definition= models.CharField(max_length=200)
-	word_id=models.CharField(max_length=100,primary_key=True)
->>>>>>> 21a288ebda0aa02d8f4a3bb5d0d2046d7290d4ce
 
-	def __str__(self):
-		return self.word_id
-	
+    def __str__(self):
+        return self.word_id
 
 
 class Progress(models.Model):
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	word_id= models.ForeignKey(WordList,on_delete=models.CASCADE)
-	learned= models.BooleanField(default=False)
-	correct=models.IntegerField(default=0)
-	wrong=models.IntegerField(default=0)
-	interval= models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    word_id= models.ForeignKey(WordList,on_delete=models.CASCADE)
+    learned= models.BooleanField(default=False)
+    correct=models.IntegerField(default=0)
+    wrong=models.IntegerField(default=0)
+    interval= models.DateTimeField(default=timezone.now)
 
-	def __str__(self):
-		return str((word_id,learned,correct,wrong))
+    def __str__(self):
+        return str((self.word_id,self.learned,self.correct,self.wrong))
