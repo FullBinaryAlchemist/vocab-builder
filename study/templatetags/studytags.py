4cde5fce6d610d4f90fe8bee3ae5012bf6e517da
Rohan_PP <rohanpjain879@gmail.com>
2019-03-09 22:54:04 +0530

    Renamed tot_words to studytags

--- study/templatetags/studytags.py
+++ study/templatetags/studytags.py
@@ -3,5 +3,4 @@ from django.template.defaultfilters import register
 
 @register.filter(name='get_value')
 def get_value(tot_words, key):
-    print(tot_words.get(key))
-    return tot_words.get(key)
+    return tot_words.get(str(key))  # since type(key)=<class 'study.models.List'>
