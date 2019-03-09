500c1f4375548168e69bd432aa56af841cb1976d
Rohan_PP <rohanpjain879@gmail.com>
2019-03-09 23:03:36 +0530

    Changes in learn and raview

--- study/views.py
+++ study/views.py
@@ -1,24 +1,50 @@
 from django.shortcuts import render, redirect
 from .models import *
 
-
 # Create your views here.
+
 
 def tostudy(request):
 	return redirect('study')
 
 
 def study(request):
-	list_type = List.objects.all()
+	# get all categories
+	categories = List.objects.all()
+
+	# get learned words
+	words_learned = 0
+
+	# get no. of ques remained for reviewing
+	tot_review_que = 0
+
 	context = {
-		'list_type': list_type,
+		'categories': categories, 'words_learned': words_learned, 'tot_review_que': tot_review_que,
 	}
 	return render(request, 'study/study.html', context)
 
 
 def learn(request):
-    # learn_words =
-    context = {
-        'learn_words': 'a'
-    }
-    return render(request, 'study/base.html', context)
+
+	if request.method == 'POST':
+		# get selected category
+		selected_cat = request.POST['category']
+
+		# get words to learn
+		words = 'a'
+
+		context = {
+			'words': words, 'selected_cat': selected_cat,
+		}
+		return render(request, 'study/learn.html', context)
+	else:
+		return redirect('study')
+
+
+def review(request):
+	# learn_words =
+	context = {
+		'learn_words': 'a'
+	}
+	return render(request, 'study/learn.html', context)
+
