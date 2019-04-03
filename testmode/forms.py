81aa189de3edfd7dc18ad72d8e6fc06e039152ce
divyanshu <dgdivyanshu@gmail.com>
2019-04-03 21:33:45 +0530

    Message pop fixed.

---
+++ testmode/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from study.models import *
+from dashboard.models import *
+from .models import *
+'''
+class QuestionForm(forms.ModelForm):
+	
+	class Meta:
+		model=Question
+		fields=('user_response')
+'''
