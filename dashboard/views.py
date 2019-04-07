0b68711d91d3fa9ee6fe4d586e4fcbef27a55bf4
Rohan_PP <rohanpjain879@gmail.com>
2019-04-08 02:40:42 +0530

    Modifed dashboard template

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,9 +7,25 @@ from testmode.urls import *
 
 # Create your views here.
 def dashboard_view(request):
-    lists = List.objects.all()
+    """lists = List.objects.all()
     tests = Tests.objects.filter(test_data__user=request.user).order_by("-test_date")[:5]
     context = {'user': request.user, 'lists': lists, 'tests': tests}
+    return render(request, 'dashboard/page.html', context)"""
+    categories = List.objects.all()
+    # get user
+    username = request.user.username
+    # get learning word number
+    if CurrentWord.objects.all().count() < categories.count():
+        for category in categories:
+            print(category)
+            CurrentWord.objects.create(user=request.user, category=category)
+    l_word_nums = CurrentWord.objects.filter(user=request.user)
+
+    print(l_word_nums)
+
+    context = {
+        'categories': categories, 'username': username, 'l_word_nums': l_word_nums,
+    }
     return render(request, 'dashboard/page.html', context)
 
 
