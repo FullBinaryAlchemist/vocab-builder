8b8723fc4b4aa21b18bdfd2d3589e5573ed5c932
Rohan_PP <rohanpjain879@gmail.com>
2019-04-08 00:10:23 +0530

    added link to logout and commented 1 line in register/views.py

--- register/views.py
+++ register/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            # messages.success(request, f'Account created for {username}!')
             return redirect('register:signup')
     else:
         form = UserRegisterForm()
